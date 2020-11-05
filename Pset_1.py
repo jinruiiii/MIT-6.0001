58b96bdead502214a9931008584271030cc1a456
jinruiiii <foojinrui123@gmail.com>
2020-11-05 16:17:50 +0800

    First Commit

---
+++ Pset_1.py
@@ -0,0 +1,14 @@
+import math
+
+# Getting the x and y inputs from user
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+
+# Printing the result of x**y
+print(f"x**y = {x**y} ")
+
+# Assigning the value of log(2, x) to the log variable
+log = math.log(2, x)
+
+# Printing the result of log(2, x)
+print(f"log(x) = {log}")
